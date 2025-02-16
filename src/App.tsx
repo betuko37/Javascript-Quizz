02363264207819639d53277da12ee3bb6f86b8d4
Jesus Zavala <betorolitos37@gmail.com>
2025-02-15 23:39:50 -0700

    theme dark and game update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { JavaScriptLogo } from "./JavaScriptLogo";
 import { Start } from './Start';
 import { useQuestionsStore } from "./store/questions";
+import { Game } from "./Game";
 
 function App() {
 const questions = useQuestionsStore(state => state.questions)
@@ -24,7 +25,7 @@ console.log(questions)
         </Stack>
 
         {questions.length === 0 && <Start/>}
-        {questions.length > 0 && <h1>Juego</h1>}
+        {questions.length > 0 && <Game/>}
 
         
 
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,13 +1,23 @@
 //import { IconButton, Stack } from "@mui/material";
-import { Card } from "@mui/material";
+import { Card, Typography } from "@mui/material";
 import { useQuestionsStore } from "./store/questions";
 import { type Question as QuestionType } from "./types";
+import SintaxHighlighter from 'react-syntax-highlighter';
+import { atomOneDarkReasonable } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+
 
 const Question = ({info}: {info: QuestionType}) =>{
     return (
-        <Card>
+
+        <Card variant="outlined" sx={{textAlign: 'left', p:2, bgcolor: '#222'}}>
 
             {/* Pregunta */}
+            <Typography variant="h5" >{info.question}</Typography>
+
+            {/* Codigo */}
+            <SintaxHighlighter  language="javascript" style={atomOneDarkReasonable}>
+                {info.code}
+            </SintaxHighlighter>
 
         </Card>
     )
